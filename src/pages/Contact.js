292c747672b37327e9ba78753f871f9df81a77af
Digvijaya <digvijayaup@gmail.com>
2023-11-14 17:20:11 +0100

    CSS updated & Contact Page Updated

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,88 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    console.log('Form submitted:', formData);
+    // Reset form fields after submission
+    setFormData({
+      name: '',
+      email: '',
+      phone: '',
+      message: ''
+    });
+  };
+
   return (
     <>
       <Navbar />
-      <div>
-      <h2>Random contact page</h2>    
-     </div>
+      <div className="contact-container">
+        <h1>Contact Us</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Your Name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Your Email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="tel"
+              className="form-control"
+              placeholder="Your Phone Number"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <textarea
+              className="form-control"
+              rows="5"
+              placeholder="Your Message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">Send</button>
+        </form>
+      </div>
     </>
- 
-  )
+  );
 }
 
-export default Contact
+export default Contact;
+
