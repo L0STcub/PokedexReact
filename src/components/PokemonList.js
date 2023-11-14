22178e6fa25b992693d3288f160cd81c7b0d1e82
Digvijaya <digvijayaup@gmail.com>
2023-11-14 13:19:37 +0100

    Pokemon List Updated

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -7,33 +7,32 @@ function PokemonList() {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const LIMIT = 9;
+
   useEffect(() => {
     const fetchData = async (offset, LIMIT) => {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${LIMIT}`);
       const data = await response.json();
       setPokemonData(data.results);
       setTotalPages(Math.ceil(data.count / LIMIT));
-      console.log(totalPages);
     };
 
     fetchData((currentPage - 1) * LIMIT, LIMIT);
-  }, [currentPage, LIMIT, totalPages]);
+  }, [currentPage, LIMIT]);
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
       setCurrentPage(currentPage + 1);
     }
   };
+
   const handlePrevPage = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
     }
   };
 
-
   return (
     <>
-
       <div className="pokemon-list-container">
         {pokemonData.map((pokemon) => (
           <Link
@@ -43,17 +42,16 @@ function PokemonList() {
           >
             <div key={pokemon.name} className="pokemon-item">
               <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split('/')[6]}.png`}
                 alt={pokemon.name}
               />
               <div className="pokemon-details">
                 <h2>{pokemon.name}</h2>
-                <p> {pokemon.url.split('/')[6]}</p>
+                <p>{pokemon.url.split('/')[6]}</p>
               </div>
             </div>
           </Link>
         ))}
-       
       </div>
       <div className="button-container">
         <div>
@@ -67,7 +65,6 @@ function PokemonList() {
           </button>
         </div>
       </div>
-
     </>
   );
 }
